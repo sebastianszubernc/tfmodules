aadc2b16319789069e04f2046f003b56301c5c27
Sebastian Szuber <sebastian.szuber@nordcloud.com>
2023-10-12 10:59:10 +0200

    Remove cluster_id from outputs

--- terraform-modules/aws/eks/outputs.tf
+++ terraform-modules/aws/eks/outputs.tf
@@ -11,8 +11,8 @@ output "cluster_certificate_authority_data" {
   value = module.eks.cluster_certificate_authority_data
 }
 
-output "cluster_id" {
-  value = module.eks.cluster_id
+output "cluster_name" {
+  value = module.eks.cluster_name
 }
 
 output "cluster_oidc_issuer_url" {
